f97a36f377049403fedf0b590e1d984a95c0834d
vijayAdam <99938785+vijayasddd@users.noreply.github.com>
2025-07-15 15:50:27 +0800

    初始化tailwind

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-red-500">Hello World</h1>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/image']
+  modules: ['@nuxt/eslint', '@nuxt/image', '@nuxtjs/tailwindcss']
 })
